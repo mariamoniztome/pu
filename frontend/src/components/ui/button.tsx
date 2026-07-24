af972f3affe9936f9b8cc286c990f28af23328a9
mariamoniztome <maria.tome@builtcolab.pt>
2026-07-24 23:08:19 +0100

    fix: update sizing of buttons

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -15,10 +15,10 @@ const buttonVariants = cva(
         link: "text-lilac-600 underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-11 px-6 py-2",
-        sm: "h-9 px-4 text-xs",
-        lg: "h-12 px-8 text-base",
-        icon: "h-11 w-11",
+        default: "h-10 px-5 py-2",
+        sm: "h-8 px-3.5 text-xs",
+        lg: "h-11 px-6 text-base",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
